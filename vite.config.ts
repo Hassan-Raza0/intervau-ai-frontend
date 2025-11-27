be0bbaf1433bcd9d54712d4450344cc6300b3fcf
Hassan-Raza0 <hassansoomroraza@gmail.com>
2025-11-26 22:19:21 -0800

    deploy commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/intervau-ai-frontend/",   // ⭐ REQUIRED — must match repo name EXACTLY
+  base: "/intervau-ai-frontend/",   
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
